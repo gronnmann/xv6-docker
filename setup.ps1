dc6293ea5d773e0e6c9dc5100f428600bced1602
gronnmann <barti.mruk@gmail.com>
2024-01-30 14:39:50 +0100

    updated powershell

--- setup.ps1
+++ setup.ps1
@@ -1,7 +1,8 @@
 git clone https://github.com/mit-pdos/xv6-riscv.git
 
-Remove-Item ./xv6-riscv/.git
-Move-Item -Path ./xv6-riscv/*.* -Destination ./
-Remove-Item ./xv6-riscv/ -Recurse
+Remove-Item ./xv6-riscv/.git -Recurse -Force
+Move-Item -Path ./xv6-riscv/* -Destination ./
+Remove-Item ./xv6-riscv/ -Recurse -Force
 
-get-content .gdbinit.tmpl-riscv | %{$_ -replace "127\.0\.0\.1","xv6-debug"}
+Rename-Item -Path .gdbinit.tmpl-riscv -NewName .gdbinit.tmpl-riscv.bak
+Get-Content .gdbinit.tmpl-riscv | %{$_ -replace "127\.0\.0\.1","xv6-debug"} | Set-Content .gdbinit.tml-riscv
