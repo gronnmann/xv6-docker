cb235e4fa5b40596d29a3f08da6c8ca49831ee0d
gronnmann <barti.mruk@gmail.com>
2024-01-30 13:30:34 +0100

    also removes old folder

--- TDT4186/setup.sh
+++ TDT4186/setup.sh
@@ -5,5 +5,7 @@ git clone https://github.com/mit-pdos/xv6-riscv.git
 mv ./xv6-riscv/README ./xv6-riscv/README_xv6
 mv -v ./xv6-riscv/* .
 
+rm -rf ./xv6-riscv
+
 # Setup gdb target connection port to the one exposed by docker
 sed -i.bak 's/127\.0\.0\.1/xv6-debug/' ./.gdbinit.tmpl-riscv
