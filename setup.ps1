c93a903e7a20a019742fe0d6f68469c8831f3b5c
gronnmann <gronnmannthecoder@gmail.com>
2024-01-30 16:18:53 +0100

    fixed powershell bug

--- setup.ps1
+++ setup.ps1
@@ -5,4 +5,4 @@ Move-Item -Path ./xv6-riscv/* -Destination ./
 Remove-Item ./xv6-riscv/ -Recurse -Force
 
 Rename-Item -Path .gdbinit.tmpl-riscv -NewName .gdbinit.tmpl-riscv.bak
-Get-Content .gdbinit.tmpl-riscv.bak | %{$_ -replace "127\.0\.0\.1","xv6-debug"} | Set-Content .gdbinit.tml-riscv
+Get-Content .gdbinit.tmpl-riscv.bak | %{$_ -replace "127\.0\.0\.1","xv6-debug"} | Set-Content .gdbinit.tmpl-riscv
